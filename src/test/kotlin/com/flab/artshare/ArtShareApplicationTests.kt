453224eda00d0aac0f9ed0a3fc5e9e95e88a43f2
gimseonjin616 <66009926+gimseonjin@users.noreply.github.com>
2023-03-13 23:10:26 +0900

    feat: Update Ktlint

--- src/test/kotlin/com/flab/artshare/ArtShareApplicationTests.kt
+++ src/test/kotlin/com/flab/artshare/ArtShareApplicationTests.kt
@@ -1,4 +1,4 @@
-package com.flab.art_share
+package com.flab.artshare
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
@@ -9,5 +9,4 @@ class ArtShareApplicationTests {
     @Test
     fun contextLoads() {
     }
-
 }
