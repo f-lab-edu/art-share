9d2571b65b0d407ccd06da6130af4b98042f2537
gimseonjin616 <66009926+gimseonjin@users.noreply.github.com>
2023-03-20 02:45:53 +0900

    feat: Apply Ktlint

--- src/main/kotlin/com/flab/artshare/config/security/UnauthorizedEntryPoint.kt
+++ src/main/kotlin/com/flab/artshare/config/security/UnauthorizedEntryPoint.kt
@@ -1,12 +1,12 @@
 package com.flab.artshare.config.security
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.http.HttpStatus
 import org.springframework.security.core.AuthenticationException
 import org.springframework.security.web.AuthenticationEntryPoint
 import org.springframework.stereotype.Component
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
-import com.fasterxml.jackson.databind.ObjectMapper
 @Component
 class UnauthorizedEntryPoint(private val objectMapper: ObjectMapper) : AuthenticationEntryPoint {
     override fun commence(
@@ -20,4 +20,3 @@ class UnauthorizedEntryPoint(private val objectMapper: ObjectMapper) : Authentic
         response?.writer?.print(objectMapper.writeValueAsString(errorMsg))
     }
 }
-
