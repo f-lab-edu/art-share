e35d793bc0f8b2dc8fc1005b58037696ca41af4b
Jaeyeon Cho <cjyeon1022@gmail.com>
2023-03-20 15:57:15 +0900

    fix : Ktlint 및 editorconfig 설정 번경

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,15 +24,14 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("com.google.firebase:firebase-admin:9.1.1")
-    implementation("com.google.firebase:firebase-admin:9.1.1")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     compileOnly("org.projectlombok:lombok")
     annotationProcessor("org.projectlombok:lombok")
     runtimeOnly("com.h2database:h2")
     runtimeOnly("com.mysql:mysql-connector-j")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("io.kotest:kotest-runner-junit5:5.3.2")
-    testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.1")
+    testImplementation("io.kotest:kotest-runner-junit5:5.4.2")
+    testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.2")
 }
 
 tasks.withType<KotlinCompile> {
@@ -106,55 +105,22 @@ koverReport {
 
     xml {
         onCheck = false
-        setReportFile(file("$rootDir/config/kover-xml-result/report.xml")) // 수정된 부분
-
-        filters {
-            excludes {
-                packages("com.flab.artshare.xml.subpackage")
-            }
-            includes {
-                packages("com.flab.artshare.xml")
-            }
-        }
+        setReportFile(file("$rootDir/config/kover-xml-result/report.xml"))
     }
 
     html {
         title = "ArtShare Coverage Report"
         onCheck = false
         setReportDir(file("$rootDir/config/kover-html-result"))
-
-        filters {
-            excludes {
-                classes("com.flab.artshare.ArtShareApplication.kt")
-            }
-            includes {
-                packages("com.flab.artshare")
-            }
-        }
     }
 
     verify {
-        onCheck = true
         rule {
-            isEnabled = true
             entity = kotlinx.kover.gradle.plugin.dsl.GroupingEntityType.APPLICATION
-
-            filters {
-                excludes {
-                    classes("com.flab.artshare.ArtShareApplication.kt")
-                }
-                includes {
-                    packages("com.flab.artshare")
-                }
-            }
 
             bound {
                 minValue = 1 // 검증할 커버리지의 최소 범위
                 maxValue = 99 // 검증할 커버리지의 최대 범위
-                // 검증할 커버리지 메트릭을 라인 커버리지로 설정. 라인 커버리지는 소스 코드의 각 줄이 테스트에서 얼마나 실행되었는지를 나타내는 메트릭
-                metric = kotlinx.kover.gradle.plugin.dsl.MetricType.LINE
-                // 커버리지 메트릭의 집계 방식을 커버된 백분율로 설정. 이는 검증할 커버리지를 백분율 값으로 계산하게 된다.
-                aggregation = kotlinx.kover.gradle.plugin.dsl.AggregationType.COVERED_PERCENTAGE
             }
         }
     }
