50a6a874dbc5b7041b4c3d76710c749b29e7dd7e
gimseonjin616 <66009926+gimseonjin@users.noreply.github.com>
2023-03-25 19:35:18 +0900

    fix: Add Valid Logic in Dto

--- src/main/kotlin/com/flab/artshare/common/dto/CreateProfileReq.kt
+++ src/main/kotlin/com/flab/artshare/common/dto/CreateProfileReq.kt
@@ -2,10 +2,18 @@ package com.flab.artshare.common.dto
 
 import com.flab.artshare.profile.Profile
 import org.springframework.web.multipart.MultipartFile
+import javax.validation.constraints.Pattern
+import javax.validation.constraints.Size
 
 data class CreateProfileReq(
+    @field:Size(max = 10, message = "Display name must be 10 characters or less")
+    @field:Pattern(regexp = "^[가-힣a-zA-Z]*$", message = "Display name must contain only Korean and English characters")
     val displayName: String,
+
+    @field:Size(max = 50, message = "About must be 50 characters or less")
+    @field:Pattern(regexp = "^[가-힣a-zA-Z0-9\\p{Punct}]*$", message = "About must contain only Korean, English characters, numbers and punctuation")
     val about: String,
+
     val profileImg: MultipartFile
 ) {
     fun toEntity(uid: String, imgPath: String): Profile {
