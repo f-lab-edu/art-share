1b5d722dcacac9df12385a207ce69c561eda2fc4
gimseonjin616 <66009926+gimseonjin@users.noreply.github.com>
2023-03-25 14:19:12 +0900

    fix : #13 lombok 의존성 삭제

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,8 +25,6 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("com.google.firebase:firebase-admin:9.1.1")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
-    compileOnly("org.projectlombok:lombok")
-    annotationProcessor("org.projectlombok:lombok")
     runtimeOnly("com.h2database:h2")
     runtimeOnly("com.mysql:mysql-connector-j")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
