7e5f41c78c20fdd911da113a0de8f51b4b3a851a
gimseonjin616 <66009926+gimseonjin@users.noreply.github.com>
2023-04-01 19:01:29 +0900

    feat: Create Reading Profile Path

--- src/main/kotlin/com/flab/artshare/profile/ProfileController.kt
+++ src/main/kotlin/com/flab/artshare/profile/ProfileController.kt
@@ -2,14 +2,12 @@ package com.flab.artshare.profile
 
 import com.flab.artshare.common.dto.CreateProfileReq
 import com.flab.artshare.common.dto.CreateProfileRes
+import com.flab.artshare.common.dto.ReadProfileRes
 import com.flab.artshare.config.swagger.docs.ExplainProfileApi
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.security.core.annotation.AuthenticationPrincipal
-import org.springframework.web.bind.annotation.ModelAttribute
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/profile")
@@ -21,5 +19,10 @@ class ProfileController(private val profileService: ProfileService) : ExplainPro
     ): ResponseEntity<CreateProfileRes> {
         val result = this.profileService.createProfile(uid, req)
         return ResponseEntity(result, HttpStatus.CREATED)
+    }
+    @GetMapping("/me")
+    override fun readProfile(@AuthenticationPrincipal uid: String): ResponseEntity<ReadProfileRes> {
+        val result = this.profileService.findProfile(uid)
+        return ResponseEntity(result, HttpStatus.OK)
     }
 }
