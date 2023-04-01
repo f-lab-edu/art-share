9cf6e9c39f185a8c6e68a14e47c68b9b1efe16c1
gimseonjin616 <66009926+gimseonjin@users.noreply.github.com>
2023-04-01 21:36:10 +0900

    feat: Create Test Code

--- src/test/kotlin/com/flab/artshare/unit/ProfileServiceTest.kt
+++ src/test/kotlin/com/flab/artshare/unit/ProfileServiceTest.kt
@@ -1,6 +1,7 @@
 package com.flab.artshare.unit
 
 import com.flab.artshare.common.dto.CreateProfileReq
+import com.flab.artshare.common.dto.UpdateProfileReq
 import com.flab.artshare.profile.Profile
 import com.flab.artshare.profile.ProfileRepository
 import io.kotest.core.spec.style.BehaviorSpec
@@ -11,8 +12,8 @@ import com.flab.artshare.storage.StorageService
 import io.kotest.core.spec.style.AnnotationSpec.Test
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.shouldNotBe
+import io.mockk.*
 import io.mockk.every
-import io.mockk.mockk
 import java.util.*
 
 @Test()
@@ -102,6 +103,75 @@ class ReadProfileTest : BehaviorSpec({
             then("throw NullPointerException") {
                 shouldThrow<NullPointerException> {
                     profileService.findProfile(uid)
+                }
+            }
+        }
+    }
+})
+
+
+@Test()
+class UpdateProfileTest : BehaviorSpec({
+    val storageService = mockk<StorageService>()
+    val profileRepo = mockk<ProfileRepository>()
+    val profileService = ProfileService(storageService, profileRepo)
+
+    val uid = "test-uid"
+
+    val bytes = "test".toByteArray()
+    val multipartFile = MockMultipartFile("file", "test.jpg", "text/plain", bytes)
+
+    given("a update profile request") {
+
+        val request1 = UpdateProfileReq("new name", null, null)
+        val request2 = UpdateProfileReq("new name", "new about", null)
+        val request3 = UpdateProfileReq("new name", "new about", multipartFile)
+
+        `when`("I update user profile successfully") {
+            val profile = Profile(1L, uid, "John", "Hi! Nice to meet you bro!", "not updated")
+            val capturedProfiles = mutableListOf<Profile>()
+
+            every { profileRepo.findByUid(any()) } returns Optional.of(profile)
+            every { storageService.uploadImage(any()) } returns "new path"
+            every { profileRepo.save(capture(slot())) } answers {
+                capturedProfiles.add(firstArg())
+                profile
+            }
+
+            println(profileService.updateProfile(uid, request1))
+            profileService.updateProfile(uid, request2)
+            profileService.updateProfile(uid, request3)
+
+            val expected1 = capturedProfiles[0]
+            val expected2 = capturedProfiles[1]
+            val expected3 = capturedProfiles[2]
+
+            println(capturedProfiles)
+
+            then("I should receive the updated profile") {
+                expected1 shouldNotBe null
+                expected1.displayName shouldBe request1.displayName
+                expected1.about shouldBe profile.about
+                expected1.imgPath shouldBe profile.imgPath
+
+                expected2 shouldNotBe null
+                expected2.displayName shouldBe request2.displayName
+                expected2.about shouldBe request2.about
+                expected2.imgPath shouldBe profile.imgPath
+
+                expected3 shouldNotBe null
+                expected3.displayName shouldBe request3.displayName
+                expected3.about shouldBe request3.about
+                expected3.imgPath shouldBe "new path"
+            }
+        }
+
+        `when`("I update not exists user profile") {
+            every { profileRepo.findByUid(any()) } returns Optional.empty()
+
+            then("throw NullPointerException") {
+                shouldThrow<NullPointerException> {
+                    profileService.updateProfile(uid, request1)
                 }
             }
         }
