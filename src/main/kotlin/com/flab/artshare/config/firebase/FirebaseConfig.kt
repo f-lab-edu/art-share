b5fb64586f17979f5de830dd92977a860221d22b
gimseonjin616 <66009926+gimseonjin@users.noreply.github.com>
2023-04-02 16:50:21 +0900

    setting: Create ConfigurationProperties About Firebase

--- src/main/kotlin/com/flab/artshare/config/firebase/FirebaseConfig.kt
+++ src/main/kotlin/com/flab/artshare/config/firebase/FirebaseConfig.kt
@@ -1,50 +1,11 @@
 package com.flab.artshare.config.firebase
 
-import com.google.auth.oauth2.GoogleCredentials
-import com.google.firebase.FirebaseApp
-import com.google.firebase.FirebaseOptions
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.internal.EmulatorCredentials
-import org.springframework.beans.factory.annotation.Value
-import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Configuration
-import java.io.File
-import java.io.FileInputStream
-import java.io.IOException
-import kotlin.properties.Delegates
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.ConstructorBinding
 
-@Configuration
-class FirebaseConfig {
-
-    @Value("\${firebase.admin-sdk.path}")
-    private lateinit var firebaseAdminSdkPath: String
-
-    @Value("\${firebase.emulator.use_emulator}")
-    private var useEmulator :Boolean = true
-
-    @Bean
-    @Throws(IOException::class)
-    fun firebaseAuth(): FirebaseAuth {
-        val credentials = getCredential()
-
-        val options = FirebaseOptions.builder()
-            .setCredentials(credentials)
-            .setProjectId("art-share")
-            .build()
-
-        if (FirebaseApp.getApps().isEmpty()) {
-            FirebaseApp.initializeApp(options)
-        }
-
-        return FirebaseAuth.getInstance()
-    }
-
-    fun getCredential(): GoogleCredentials {
-        return if (useEmulator)
-            EmulatorCredentials()
-        else {
-            val file = FileInputStream(File(firebaseAdminSdkPath))
-            GoogleCredentials.fromStream(file)
-        }
-    }
-}
+@ConstructorBinding
+@ConfigurationProperties(prefix = "firebase")
+data class FirebaseConfig(
+    var adminSdkPath: String = "dummy",
+    var useEmulator :Boolean = true
+)
