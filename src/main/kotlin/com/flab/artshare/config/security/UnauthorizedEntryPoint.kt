5618932f8d82ac903405ee336e8b3da6c905529d
gimseonjin616 <66009926+gimseonjin@users.noreply.github.com>
2023-04-02 13:30:39 +0900

    Revert "fix: Refactor UnauthorizedEntityPint"
    This reverts commit 033286ee0b3f2b895b72eeeab28c0b44a7a2e0a7.

--- src/main/kotlin/com/flab/artshare/config/security/UnauthorizedEntryPoint.kt
+++ src/main/kotlin/com/flab/artshare/config/security/UnauthorizedEntryPoint.kt
@@ -2,7 +2,6 @@ package com.flab.artshare.config.security
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.http.HttpStatus
-import org.springframework.http.MediaType
 import org.springframework.security.core.AuthenticationException
 import org.springframework.security.web.AuthenticationEntryPoint
 import org.springframework.stereotype.Component
@@ -11,19 +10,14 @@ import javax.servlet.http.HttpServletResponse
 
 @Component
 class UnauthorizedEntryPoint(private val objectMapper: ObjectMapper) : AuthenticationEntryPoint {
-    private val errorMsg = mapOf("code" to HttpStatus.UNAUTHORIZED.value(), "msg" to "Unauthorized")
-
     override fun commence(
         request: HttpServletRequest?,
         response: HttpServletResponse?,
         authException: AuthenticationException?
     ) {
-        response?.apply {
-            contentType = MediaType.APPLICATION_JSON_VALUE
-            status = HttpStatus.UNAUTHORIZED.value()
-            writer.use { writer ->
-                objectMapper.writeValue(writer, errorMsg)
-            }
-        }
+        val errorMsg = mapOf("code" to HttpStatus.UNAUTHORIZED.value(), "msg" to "Unauthorized")
+        response?.contentType = "application/json;charset=UTF-8"
+        response?.status = HttpStatus.UNAUTHORIZED.value()
+        response?.writer?.print(objectMapper.writeValueAsString(errorMsg))
     }
 }
