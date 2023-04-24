61e91619c5acc96235add411a76c3ec4b4a39745
gimseonjin616 <66009926+gimseonjin@users.noreply.github.com>
2023-04-24 19:05:56 +0900

    feat: Delete Test Yaml

--- art-external/src/main/kotlin/com/flab/artshare/naverCloud/NaverCloudConfig.kt
+++ art-external/src/main/kotlin/com/flab/artshare/naverCloud/NaverCloudConfig.kt
@@ -6,9 +6,9 @@ import org.springframework.boot.context.properties.ConstructorBinding
 @ConstructorBinding
 @ConfigurationProperties(prefix = "cloud.naver")
 data class NaverCloudConfig(
-    val accessKey: String,
-    val secretKey: String,
-    val region: String,
-    val endpoint: String,
-    val bucket: String,
+    val accessKey: String = "dummy_key",
+    val secretKey: String = "dummy_key",
+    val region: String = "dummy_region",
+    val endpoint: String = "dummy_end_point",
+    val bucket: String = "dummy_bucket",
 )
