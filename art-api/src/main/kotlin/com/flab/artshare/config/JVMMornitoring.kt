cfa771892dd9060ccbfbd7a4a18f4d78a59ea040
gimseonjin616 <66009926+gimseonjin@users.noreply.github.com>
2023-06-04 15:56:19 +0900

    feat: Add JVM Config

---
+++ art-api/src/main/kotlin/com/flab/artshare/config/JVMMornitoring.kt
@@ -0,0 +1,20 @@
+package com.flab.artshare.config
+
+import io.micrometer.core.instrument.MeterRegistry
+import org.springframework.beans.factory.annotation.Value
+
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer
+import org.springframework.context.annotation.Bean
+
+
+class JVMMornitoring {
+    @Bean
+    fun configurer(
+        @Value("\${spring.application.name}") applicationName: String?,
+    ): MeterRegistryCustomizer<MeterRegistry>? {
+        return MeterRegistryCustomizer { registry: MeterRegistry ->
+            registry.config().commonTags("application", applicationName)
+        }
+    }
+}
+
