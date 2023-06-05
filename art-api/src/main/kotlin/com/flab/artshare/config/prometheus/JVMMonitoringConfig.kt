c12f80e8545cfda8cc7bf2a36d3d3925809fd3a7
gimseonjin616 <66009926+gimseonjin@users.noreply.github.com>
2023-06-05 12:21:03 +0900

    feat: Separate yaml to github secret

--- art-api/src/main/kotlin/com/flab/artshare/config/prometheus/JVMMonitoringConfig.kt
+++ art-api/src/main/kotlin/com/flab/artshare/config/prometheus/JVMMonitoringConfig.kt
@@ -1,11 +1,11 @@
-package com.flab.artshare.config
+package com.flab.artshare.config.prometheus
 
 import io.micrometer.core.instrument.MeterRegistry
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer
 import org.springframework.context.annotation.Bean
 
-class JVMMornitoring {
+class JVMMonitoringConfig {
     @Bean
     fun configurer(
         @Value("\${spring.application.name}") applicationName: String?,
