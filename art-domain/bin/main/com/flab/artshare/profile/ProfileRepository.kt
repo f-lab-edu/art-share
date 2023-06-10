67ef2723242c35c068d47f2136554d1713858f54
gimseonjin616 <66009926+gimseonjin@users.noreply.github.com>
2023-06-10 17:22:33 +0900

    feat: Delete Unneccerry code and fix kotlint

--- art-domain/bin/main/com/flab/artshare/profile/ProfileRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.flab.artshare.profile
-
-import org.springframework.data.jpa.repository.JpaRepository
-import java.util.*
-
-interface ProfileRepository : JpaRepository<Profile, Long> {
-    fun existsByDisplayNameOrAbout(displayName: String, about: String): Boolean
-    fun existsByUid(uid: String): Boolean
-}
--- art-external/bin/main/com/flab/artshare/firebase/FirebaseApi.kt
+++
@@ -1,9 +0,0 @@
-package com.flab.artshare.firebase
-
-import com.google.firebase.auth.FirebaseAuth
-import org.springframework.stereotype.Service
-
-@Service
-class FirebaseApi(private val firebaseAuth: FirebaseAuth) {
-    fun verifyToken(token: String): String = this.firebaseAuth.verifyIdToken(token).uid
-}
