cd1f8687b86a6245948ac573b167053efa47c9a2
gimseonjin616 <66009926+gimseonjin@users.noreply.github.com>
2023-06-10 19:07:03 +0900

    feat: Fix Codes

--- art-domain/src/main/resources/migration/V1__init.sql
+++ art-domain/src/main/resources/migration/V1__init.sql
@@ -1,4 +1,4 @@
-CREATE TABLE Profile (
+CREATE TABLE profile (
     id bigint not null auto_increment,
     uid VARCHAR(255) NOT NULL,
     displayName VARCHAR(255) NOT NULL,
@@ -7,10 +7,12 @@ CREATE TABLE Profile (
     primary key (id)
 ) engine=InnoDB;
 
-CREATE TABLE Post (
+CREATE TABLE post (
     id bigint not null auto_increment,
     created_at datetime(6) not null,
     updated_at datetime(6) not null,
+    created_by varchar(15) not null,
+    updated_by varchar(15) not null,
     content varchar(255) not null,
     title varchar(255) not null,
     primary key (id)
