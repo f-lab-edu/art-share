0f3a661ceb74b1ad949e2e6b7adf34f39265817f
gimseonjin616 <66009926+gimseonjin@users.noreply.github.com>
2023-06-10 18:14:29 +0900

    fix: Update Migration file

--- art-domain/src/main/resources/migration/V1__init.sql
+++ art-domain/src/main/resources/migration/V1__init.sql
@@ -1,12 +1,17 @@
---create table PERSON (
---    ID int not null,
---    NAME varchar(100) not null
---);
-
 CREATE TABLE Profile (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    id bigint not null auto_increment,
     uid VARCHAR(255) NOT NULL,
     displayName VARCHAR(255) NOT NULL,
     about VARCHAR(255) NOT NULL,
     imgPath VARCHAR(255) NOT NULL
-);
+    primary key (id)
+) engine=InnoDB;
+
+CREATE TABLE Post (
+    id bigint not null auto_increment,
+    created_at datetime(6) not null,
+    updated_at datetime(6) not null,
+    content varchar(255) not null,
+    title varchar(255) not null,
+    primary key (id)
+) engine=InnoDB;
